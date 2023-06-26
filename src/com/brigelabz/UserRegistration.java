39a27049c43b401c0a877564b316cfea1be5e0cb
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 22:29:19 +0530

    Add: Code for validation of last name is added.

--- src/com/brigelabz/UserRegistration.java
+++ src/com/brigelabz/UserRegistration.java
@@ -5,9 +5,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("[A-z][a-z]{3,}");
-        Matcher m = p.matcher("Sanyukta");
-        boolean result = m.matches();
+        Pattern firstName = Pattern.compile("[A-z][a-z]{3,}");
+        Matcher f = firstName.matcher("Sanyukta");
+        boolean result = f.matches();
         System.out.println(result);
+
+        Pattern lastName = Pattern.compile("[A-z][a-z]{3,}");
+        Matcher l = lastName.matcher("Berad");
+        boolean result1 = l.matches();
+        System.out.println(result1);
     }
 }
