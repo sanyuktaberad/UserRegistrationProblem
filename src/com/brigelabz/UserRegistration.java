911dc14ee93259fa29970d1fd31772c0de6fd5a9
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 23:46:22 +0530

    Add: Code for validation of email is added with atleast one capital letter.

--- src/com/brigelabz/UserRegistration.java
+++ src/com/brigelabz/UserRegistration.java
@@ -32,8 +32,8 @@ public class UserRegistration {
         boolean result3 = m.matches();
         System.out.println(result3);
 
-        Pattern password = Pattern.compile("[A-za-z]{8,}");
-        Matcher p = password.matcher("Password");
+        Pattern password = Pattern.compile("[A-za-z]+");
+        Matcher p = password.matcher("passWord");
         boolean result4 = p.matches();
         System.out.println(result4);
     }
